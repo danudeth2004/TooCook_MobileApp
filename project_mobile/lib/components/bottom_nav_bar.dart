14db89b1d36b12db4bfd63b623e62c08c84a3f4a
danudeth2004 <oat25482547@gmail.com>
2025-04-14 04:22:44 +0700

    fix tab

--- project_mobile/lib/components/bottom_nav_bar.dart
+++ project_mobile/lib/components/bottom_nav_bar.dart
@@ -1,52 +1,52 @@
-  import 'package:flutter/material.dart';
-  import 'package:project_mobile/contact.dart';
-  import 'package:project_mobile/homepage.dart';
-  import 'package:project_mobile/random_menu.dart';
+import 'package:flutter/material.dart';
+import 'package:project_mobile/contact.dart';
+import 'package:project_mobile/homepage.dart';
+import 'package:project_mobile/random_menu.dart';
 
   //เป็น component BottomNavigationBar แบบ StatelessWidget
-  class BottomNavBar extends StatelessWidget {
-    final int selectedIndex;
+class BottomNavBar extends StatelessWidget {
+  final int selectedIndex;
 
-    const BottomNavBar({super.key, required this.selectedIndex});
+  const BottomNavBar({super.key, required this.selectedIndex});
 
-    // ฟังก์ชันนี้จะถูกเรียกเมื่อผู้ใช้แตะที่ไอคอนใน BottomNavigationBar และมีการสร้าง Route ไปยังหน้าที่ index
-    void _onItemTapped(BuildContext context, int index) {
+  // ฟังก์ชันนี้จะถูกเรียกเมื่อผู้ใช้แตะที่ไอคอนใน BottomNavigationBar และมีการสร้าง Route ไปยังหน้าที่ index
+  void _onItemTapped(BuildContext context, int index) {
 
-      final List<Widget> destinations = [
-        const RandomMenu(),
-        const Homepage(),
-        const Contact(),
-      ];
+    final List<Widget> destinations = [
+      const RandomMenu(),
+      const Homepage(),
+      const Contact(),
+    ];
 
-      Navigator.of(context).pushAndRemoveUntil(
-        MaterialPageRoute(builder: (_) => destinations[index]),
-        (route) => false,
-      );
-    }
+    Navigator.of(context).pushAndRemoveUntil(
+      MaterialPageRoute(builder: (_) => destinations[index]),
+      (route) => false,
+    );
+ }
     
-    //สร้าง BottomNavigationBar ประกอบไปด้วย Random, home, contact โดย currentIndex หรือหน้าจอที่จะแสดงผลจะถูกส่งมาจากหน้าที่เรียกใช้ component นี้
-    @override
-    Widget build(BuildContext context) {
-      return BottomNavigationBar(
-        currentIndex: selectedIndex,
-        onTap: (index) => _onItemTapped(context, index),
-        backgroundColor: Colors.white,
-        unselectedItemColor: Colors.grey,
-        selectedItemColor: Theme.of(context).colorScheme.primary,
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.fastfood),
-            label: 'Random',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home'
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.contact_page),
-            label: 'Contact',
-          ),
-        ],
-      );
-    }
+  //สร้าง BottomNavigationBar ประกอบไปด้วย Random, home, contact โดย currentIndex หรือหน้าจอที่จะแสดงผลจะถูกส่งมาจากหน้าที่เรียกใช้ component นี้
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      currentIndex: selectedIndex,
+      onTap: (index) => _onItemTapped(context, index),
+      backgroundColor: Colors.white,
+      unselectedItemColor: Colors.grey,
+      selectedItemColor: Theme.of(context).colorScheme.primary,
+      items: const [
+        BottomNavigationBarItem(
+          icon: Icon(Icons.fastfood),
+          label: 'Random',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.home),
+          label: 'Home'
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.contact_page),
+          label: 'Contact',
+        ),
+      ],
+    );
   }
+}
